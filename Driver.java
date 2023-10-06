c8e702f23a6a026c070ff11b54ebf3f07266d48b
thomasg13 <121275852+thomasg13@users.noreply.github.com>
2023-10-06 12:44:38 -0400

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -2,18 +2,24 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class Driver{
    public static void main(String[] args){
       JFrame frame = new JFrame("Display");
       Board c = new Board();
-      frame.setSize(1000, 1000);
+      Yo y = new Yo();
+      frame.setSize(2000, 1000);
       frame.setResizable(false);
       frame.setLocation(0, 0);
-      frame.setContentPane(c);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setVisible(true);
+      frame.setLayout(new BorderLayout());
+      frame.add(c, BorderLayout.WEST);
+      frame.add(y, BorderLayout.CENTER);
       //frame.getContentPane().add(b, BorderLayout.EAST);
       c.start();
+      y.start();
+      //SwingUtilities.invokeLater(() -> new FrameWithTwoPanels());
    }
 }
