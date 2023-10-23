016c37a4122f7d2459ebd145ddc578d94d519d0a
evhuynh21 <130769844+evhuynh21@users.noreply.github.com>
2023-10-23 13:15:25 -0400

    Add files via upload

--- Yo.java
+++ Yo.java
@@ -6,72 +6,23 @@ public class Yo extends JPanel{
    private static Cell[][] cells = new Cell[DIM][DIM];
    private static int x1, x2, x3, x4, x5, y1, y2, y3, y4, y5;
    
+   private static Button[] buttons = new Button[12];
+   private static Shape r1 = new Rectangle(10, 10, 130, 55);
+   
+   public void buttonSetup(){
+        buttons[0] = new Button(r1, "Brute Force", Color.YELLOW, Color.decode("#B67949"), Color.BLACK);
+   }
+   
    public Yo(){
-      setLayout(new GridLayout(DIM, DIM));
-      for (int r = 0; r < cells.length; r++){
-         for (int c = 0; c < cells[0].length; c++){
-            cells[r][c] = new Cell(r, c);
-            add(cells[r][c]);
-            cells[r][c].setBackground(new Color(70, 240, 110)); // (70, 240, 110) for a good green
-            cells[r][c].setEnabled(false);
-         }
-      }
-   }
+      buttonSetup();
+   }      
    
-   public static void start(){
-      int places = 20;//change this
-      int edges = 8;
-      int indexes[][] = new int[places][2];
-      for(int i = 0; i < places; i ++){
-         int a = (int)(Math.random() * DIM);
-         int b = (int)(Math.random() * DIM);
-         if(!cells[a][b].getActivated()){
-            cells[a][b].activate(i + 1);
-            cells[a][b].setBackground(Color.WHITE);
-            /*if(i == 0){
-               cells[a][b].setBackground(Color.GREEN);
-            }
-            if(i == places - 1){
-               cells[a][b].setBackground(Color.RED);
-            }*/
-            indexes[i][0] = cells[a][b].getX() + 25;
-            indexes[i][1] = cells[a][b].getY() + 25;         
-         }else{
-            i --;
-         }
-      }  
-      x1 = indexes[0][0];
-      y1 = indexes[0][1];
-      x2 = indexes[1][0];
-      y2 = indexes[1][1];
-      x3 = indexes[2][0];
-      y3 = indexes[2][1];
-      x4 = indexes[3][0];
-      y4 = indexes[3][1];
-      x5 = indexes[4][0];
-      y5 = indexes[4][1];
-      /*for (int r = 0; r < cells.length; r++){
-         for (int c = 0; c < cells[0].length; c++){
-            if(!cells[r][c].getActivated()){
-               cells[r][c].deactivate();
-            }
-         }
-      }*/
-   }
-   
-   
-   /*
    @Override 
    public void paint(Graphics g) {
       super.paint(g);
       Graphics2D g2 = (Graphics2D) g;
-      g2.setStroke(new BasicStroke(3));
-      g.drawLine(x1, y1, x2, y2);
-      g.drawLine(x2, y2, x3, y3);
-      g.drawLine(x3, y3, x4, y4);
-      g.drawLine(x4, y4, x5, y5);
-      g.drawLine(x5, y5, x1, y1);
-   }*/
+      buttons[0].drawButton(g);
+   }
  
    /*  
    public static void main(String[] args){
